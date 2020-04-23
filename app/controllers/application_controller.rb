774ca3aa5644d4b1c253cbd3a6ea665b4a77e5b5
yoheyx2 <62735469+yoheyx2@users.noreply.github.com>
2020-04-23 21:12:49 +0900

    basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,14 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def production?
-    Rails.env.production?
-  end
-
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == Rails.application.credentials[:basic_auth][:user] &&
       password == Rails.application.credentials[:basic_auth][:pass]
     end
+  end
+
+  def production?
+    Rails.env.production?
   end
 end
