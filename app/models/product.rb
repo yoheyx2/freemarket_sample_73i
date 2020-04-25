78b10911669c117085c1a8a352ff94b186435789
tomoki828 <pico4828@gmail.com>
2020-04-25 10:19:05 +0900

    carrierwave,mini_magickの導入
    画像投稿機能の実装のため上記gemを導入した。
    また、product_imagesテーブルに画像を保存するための基礎を作成した。

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,10 @@ class Product < ApplicationRecord
   validates :name, presence: true, length: { maximum: 40 }
   validates :infomation, presence: true, length: { maximum: 1000 }
   validates :brand, presence: true
+
+  has_many :product_images
+
+  accepts_nested_attributes_for :product_images
+
+  mount_uploader :image, ImageUploader
 end
