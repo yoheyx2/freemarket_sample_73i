bbdacfa4206d47f99804b93d23260883cc00f804
Yu-chan1023 <minami.15801580@gmail.com>
2020-04-27 12:58:53 +0900

    Unnecessary file destroy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
-  before_action :get_category
   private
 
   def production?
@@ -13,10 +12,5 @@ class ApplicationController < ActionController::Base
       password == Rails.application.credentials[:basic_auth][:pass]
     end
   end
- 
-  def get_category
-    @categories = Category.all
-    @parents = Category.all.order("id ASC")  
-  end
-  
+
 end
