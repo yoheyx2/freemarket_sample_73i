4a2f1ee405a0d7f89b26f907e256a2491e1a8f8b
tomoki828 <pico4828@gmail.com>
2020-04-27 19:40:11 +0900

    単一の画像投稿機能の実装。
    一枚画像投稿ができる機能を実装した。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,16 +7,18 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
+    @product.product_images.build
   end
 
   def create
+    binding.pry
     Product.create(product_params)
     redirect_to root_path
   end
 
   private
   def product_params
-    params.require(:product).permit(:name, :infomation, :brand, :status, :delivery_fee, :ship_form, :delivery_time, :price, :category, product_image_attributes: [:id, :image])
+    params.require(:product).permit(:name, :infomation, :brand, :status, :delivery_fee, :ship_form, :delivery_time, :price, :category, product_images_attributes: [:image] )
     # 「:category」は「:category_id」に変更となる予定
     # ユーザー登録機能実装後は「.merge(user_id: current_user.id)」を記述すること。
   end
--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -1,3 +1,4 @@
 class ProductImage < ApplicationRecord
-  belongs_to :product
+  belongs_to :product, optional: true
+  mount_uploader :image, ImageUploader
 end
