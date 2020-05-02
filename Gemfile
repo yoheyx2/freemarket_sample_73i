e71c3afdf90f02eb5f94b98c283ffc91d3196b0a
tomoki828 <pico4828@gmail.com>
2020-05-02 16:38:45 +0900

    jqueryによる販売手数料及び販売利益の自動表示機能追加
    税込価格として表示予定だったが、メルカリと仕様を合わせる形で販売手数料及び販売利益の自動表示をする形に直した。
    記述はjqueryで行い、数字が0の場合は"ー"が表示され、それ以外の時には自動計算される仕様とした。

--- Gemfile
+++ Gemfile
@@ -78,6 +78,7 @@ gem 'rspec-rails'
 gem 'rails-controller-testing'
 gem 'factory_bot_rails'
 gem 'faker'
+gem 'jquery-rails'
 gem "font-awesome-sass"
 gem 'devise'
 gem 'fog-aws'
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -9,7 +9,6 @@ describe ProductsController do
         post :create,
         params: params
       }
-
       it 'productとproduct_imageを保存できること' do
         expect{ subject }.to change(Product, :count).by(1)
         expect{ subject }.to change(Product_image, :count).by(1)
