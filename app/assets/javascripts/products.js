6c0f75799dd06175c8654222dd08bac8cb7a31ee
tomoki828 <pico4828@gmail.com>
2020-05-05 15:18:47 +0900

    複数画像投稿機能の実装（画像投稿のみ。プレビュー未実装。）
    画像投稿を複数行えるようにproducts.jsを記述。
    プレビューは未実装で、ビューの調整も後ほど改めて行う。

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,5 +1,27 @@
 $(function() {
 
+  function buildFileField(index) {
+    let html = `<div class="sell__product__image__box__data" data-index="${index}">
+    <label class="sell__product__image__box__data__upload" for="product_product_images_attributes_${index}_image"><input accept="image/png,image/jpeg" style="display:none" class="sell__product__image__box__data__upload--js-file" type="file" name="product[product_images_attributes][${index}][image]" id="product_product_images_attributes_${index}_image">
+    <span class="sell__product__image__box__data__upload--js-remove">削除</span>
+    </label></div>`;
+    return html;
+  }
+
+  let fileIndex = [1,2,3,4,5,6,7,8,9,10];
+
+  $('.sell__product__image__box').on('change', '.sell__product__image__box__data__upload--js-file', function(e) {
+    $('.sell__product__image__box').append(buildFileField(fileIndex[0]));
+    fileIndex.shift();
+    fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+  });
+
+  $('.sell__product__image__box').on('click', '.sell__product__image__box__data__upload--js-remove', function() {
+    $(this).parent().remove();
+    if ($('.sell__product__image__box__data__upload--js-file').length == 0) $('.sell__product__image__box').append(buildFileField(fileIndex[0]));
+  });
+
+
   $('.sell__product__price--input').change(function() {
     let commission = 10; 
     let value = $(this).val();
@@ -14,5 +36,5 @@ $(function() {
       $('.sell__product__fee__profit--js').text("¥" + value_profit);  
     }
   });
-  
+
 });
