83cbf537c6d6931129d55ee06e1d7564d0bdb6b6
kaimasa1115 <62590844+kaimasa1115@users.noreply.github.com>
2020-05-11 11:43:27 +0900

    testcode

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,8 @@ class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to :user
+  validates :postal_code, presence: true
+  validates :prefecture_id, presence: true
+  validates :city, presence: true
+  validates :address, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          has_many :addresses
          accepts_nested_attributes_for :addresses
+         validates :nickname, presence: true
+         validates :email, presence: true
+         validates :password, presence: true, length: {minimum: 7}
+         validates :password_confirmation, presence: true, length: {minimum: 7}
+         validates :first_name, presence: true
+         validates :last_name, presence: true
+         validates :first_name_furigana, presence: true
+         validates :last_name_furigana, presence: true
+         validates :date_of_birth, presence: true
 end
