fe679edaabda501ddacb32ff41d11acf74dea1de
tomoki828 <pico4828@gmail.com>
2020-05-12 18:13:44 +0900

    マイページリンクのエラー解消
    controllerの記述変更によってリンクがエラーになっていたので解消した。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def index
-    @contents = ["マイページ","お知らせ","いいね！一覧","出品する","出品した商品","購入した商品"]
-    @settings = ["プロフィール","支払い方法","メール/パスワード","本人情報","ログアウト"]
+    @contents = [{name:"マイページ",path: "/users",pattern: "GET"},{name:"お知らせ",path: "",pattern: ""},{name:"いいね一覧",path: "",pattern: ""},{name:"出品する",path: "/products/new",pattern: "GET"},{name:"出品した商品",path: "",pattern: ""},{name:"購入した商品",path: "",pattern: ""}]
+    @settings = [{name:"プロフィール",path: "",pattern: ""},{name:"支払方法",path: "/cards/new",pattern: "GET"},{name:"メール/パスワード",path: "",pattern: ""},{name:"本人情報",path: "",pattern: ""},{name:"ログアウト",path: "/users/sign_out",pattern: "DELETE"}]
     @product_quantity = Product.where(user_id: current_user.id).count
   end
 end
