448ffc5c173e0b90817e40acf84ea3c8e6caf043
yoheyx2 <62735469+yoheyx2@users.noreply.github.com>
2020-05-13 12:35:06 +0900

    購入機能におけるCardモデルの単体テスト

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :card do
-    
+    user            { create(:user) }
+    customer_id     {"cus_11111111111111aaaaaaaaaaaaaa"}
+    card_id         {"car_11111111111111aaaaaaaaaaaaaa"}
   end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
+describe Card do
+  describe '#create' do
 
-RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    context '登録できる場合' do
+      it "user_idとcustomer_idとcard_idがある場合は登録できること" do
+        card = build(:card)
+        expect(card).to be_valid
+      end
+    end
+
+    context '登録できない場合' do
+      it "user_idがない場合は登録できないこと" do
+        card = build(:card, user_id: nil)
+        card.valid?
+        expect(card.errors[:user_id]).to include("can't be blank")
+      end
+
+      it "customer_idがない場合は登録できないこと" do
+        card = build(:card, customer_id: nil)
+        card.valid?
+        expect(card.errors[:customer_id]).to include("can't be blank")
+      end
+
+      it "card_idがない場合は登録できないこと" do
+        card = build(:card, card_id: nil)
+        card.valid?
+        expect(card.errors[:card_id]).to include("can't be blank")
+      end
+    end
+  end
 end
